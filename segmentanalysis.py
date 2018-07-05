e5c1c62b7e8525609728136020f83cade1e2a4c1
Gennadii <Gennadiy.Zakharov@gmail.com>
2018-07-05 18:54:33 +0300

    Implemeted fragment search using regexp

--- segmentanalysis.py
+++ segmentanalysis.py
@@ -12,9 +12,10 @@ import argparse
 import glob
 import sys
 from os.path import join, abspath, curdir
+
 sys.path.append(abspath(join(curdir, 'segmentanalysis')))
 from segmentanalysis.segmentutils import revcomp
-
+from segmentanalysis import segmentsearch
 
 # 0. ANALYZING INPUT PARAMETERS
 
@@ -23,12 +24,14 @@ parser.add_argument("chromosomeseq", type=str,
                     help="TXT file containing chromosome sequence (one string, small letters, no name, no spaces)")
 parser.add_argument("segment", type=str,
                     help="segment of chromosome to analyze (16113516:16900779) in BED-file notation (starting from 0, end is not included)")
+parser.add_argument("-v", "--verbose", action='store_true',
+                    help='Print additional information to stdout')
 parser.add_argument("-s", "--fragmentsizes", type=str, default='5,10,15,20,25,30',
                     help='Set of fragment sizes to search')
-parser.add_argument("-d", "--fragmentdensity", type=int, default=25,
+parser.add_argument("-d", "--fragmentdensity", type=int, default=100,
                     help='Average frequency of fragments in letters Default 25 meand that in average each 25-th letter will be start of fragment')
 
-parser.add_argument("-c", "--chunk", type=int, default=10000,
+parser.add_argument("-c", "--chunk", type=int, default=10,
                     help='Chunk size to divide chromosome')
 parser.add_argument("-i", "--iterations", type=int, default=10,
                     help='Number of iterations for fragment splitting')
@@ -37,244 +40,59 @@ args = parser.parse_args()
 # Processign command-line arguments
 start, stop = [int(coord) for coord in args.segment.split(':')]
 fragmentSizes = [int(size) for size in args.fragmentsizes.split(',')]
+chunkLen = args.chunk * 1000
 
 # I. INPUT FILES PREPARATION
 
 # 1. Selection of chromosome segment 0 (to be fragmented)
 with open(args.chromosomeseq) as chrSeqFile:
-    chromosome=''.join( [line.strip() for line in chrSeqFile.readlines()] ).lower()
+    chromosome = ''.join([line.strip() for line in chrSeqFile.readlines()]).lower()
     segment = chromosome[start:stop]
-#    f2 = open(r"Segment.txt", "w")  # File containing segment sequence
-#    f2.write(segment)
-#    f2.close()
 
 # 2. Preparation of complementary segment
 segmentRevComp = revcomp(segment)
-#f = open(r"Segment.txt", "r")
-#f2 = open(r"c-Segment.txt", "w")
-
-
-# 3. Chromosome segmentation by chunks
-
-# Fragments files mask
-fileSuffix = '-x-fr.txt'
-filesMask = '*' + fileSuffix
-resultFiles = glob.glob(filesMask)
-
-begin = 0  # the firts part start
-
-# Sequence segmentation
-fragments = open(r"x-small.txt", "r")  # Chromosome sequence to be segmented
-
-for line in fragments:
-    x = str(line)
-    totallength = len(line)  # the length of x (in bases)
-    seglength = args.chunk # the length of chunk (in bases)
-    number = int(totallength / seglength)  # the number of parts
-    for i in range(1, number + 1):
-        outputFileName = str(i) + fileSuffix
-        outputFile = open(outputFileName, "w")  # File with fragments
-        end = begin + seglength  # chromosome part end
-        seq = x[begin:end]  # chromosome part sequence
-        outputFile.write(seq)  # random fragment sequence output
-        begin += seglength  # the new part start
-        outputFile.close()
-fragments.close()
 
 # II. SEARCH OF MATCHING FRAGMENTS
 
-# For the direct chromosome DNA chain:
 
-for i in range(iterations):  # 10 iterations
-
+for iteration in range(args.iterations):  # iterations of segment splitting
+    if args.verbose:
+        print('Starting split iteration: ', iteration)
     for fragmentSize in fragmentSizes:
-        pass
+        if args.verbose:
+            print('Fragment size: ', fragmentSize)
+        # For the direct chromosome DNA chain:
+        fragmentsFor = segmentsearch.chooseFragments(segment, fragmentSize, args.fragmentdensity)
+        countsFor = segmentsearch.countFragments(chromosome, fragmentsFor, args.verbose)
+        # For the reverse chromosome DNA chain:
+        fragmentsRev = segmentsearch.chooseFragments(segmentRevComp, fragmentSize, args.fragmentdensity)
+        countsRev = segmentsearch.countFragments(chromosome, fragmentsRev, args.verbose)
 
-
-    match = open(r"5-dir.txt", "a+")  # 5 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    exec (open("./Search scripts/5-dir-restricted.py").read())
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"10-dir.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    exec (open("./Search scripts/10-dir-restricted.py").read())
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"15-dir.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"15-fragments-d.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/15-dir.py").read())
-    fragments = open(r"15-fragments-d.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"20-dir.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"20-fragments-d.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/20-dir.py").read())
-    fragments = open(r"20-fragments-d.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"25-dir.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"25-fragments-d.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/25-dir.py").read())
-    fragments = open(r"25-fragments-d.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"30-dir.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"30-fragments-d.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/30-dir.py").read())
-    fragments = open(r"30-fragments-d.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
-
-# For the reverse chromosome DNA chain:
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"5-rev.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    exec (open("./Search scripts/5-rev-restricted.py").read())
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"10-rev.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    exec (open("./Search scripts/10-rev-restricted.py").read())
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"15-rev.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"15-fragments-r.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/15-rev.py").read())
-    fragments = open(r"15-fragments-r.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"20-rev.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"20-fragments-r.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/20-rev.py").read())
-    fragments = open(r"20-fragments-r.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"25-rev.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"25-fragments-r.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/25-rev.py").read())
-    fragments = open(r"25-fragments-r.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
-
-for i in range(1, 11):  # 10 iterations
-    match = open(r"30-rev.txt", "a+")  # 15 b matching fragments numbers
-    match.write("Iteration ")
-    match.write(str(i))
-    match.write("\n")
-    match.close()
-    fragments = open(r"30-fragments-r.txt", "a+")  # 15 b matching fragments sequences and positions
-    fragments.write("Iteration ")
-    fragments.write(str(i))
-    fragments.write("\n")
-    fragments.close()
-    exec (open("./Search scripts/30-rev.py").read())
-    fragments = open(r"30-fragments-r.txt", "a+")
-    fragments.write("\n")
-    fragments.close()
+exit(0)
 
 # III. OUTPUT FILES PREPARATION FOR ANALYSIS (SORTING)
-exec (open(
+exec(open(
     "./Sorting scripts/Sorting-initial-fragments-dir.py").read())  # Sorts all random (matching and dismatching) fragments according to their positions in segment
 # (direct DNA chain). Input file:*-dir.all.txt, output file: *-dir-all.sort.txt
-exec (open("./Sorting scripts/Sorting-initial-fragments-rev.py").read())  # Does the same for reverse DNA chain
-exec (open(
+exec(open("./Sorting scripts/Sorting-initial-fragments-rev.py").read())  # Does the same for reverse DNA chain
+exec(open(
     "./Sorting scripts/Sorting-matching-fragments-dir.py").read())  # Sorts matching fragments according to their positions in segment and chromosome (direct DNA chain)
 # Input file: *-d.txt, output file: *-d-list.txt *-d-count.txt
-exec (open("./Sorting scripts/Sorting-matching-fragments-rev.py").read())  # Does the same for reverse DNA chain
-exec (open(
+exec(open("./Sorting scripts/Sorting-matching-fragments-rev.py").read())  # Does the same for reverse DNA chain
+exec(open(
     "./Sorting scripts/Sorting-chromosome-segments-dir.py").read())  # Sorts numbers of matching fragments according to chromosome segment numbers
 # and calculates average number normalized to the total fragments number (direct DNA chain)
-exec (open("./Sorting scripts/Sorting-chromosome-segments-rev.py").read())  # Does the same for reverse DNA chain
+exec(open("./Sorting scripts/Sorting-chromosome-segments-rev.py").read())  # Does the same for reverse DNA chain
 
 # IV. ANALYSIS
 # Table with the normalized average numbers of matching segments
-exec (open(
+exec(open(
     "./Analysis scripts/Normalization-table-dir.py").read())  # Generates table of normalized fragment frequencies of each length for each chromosome segment (direct DNA strain)
-exec (open("./Analysis scripts/Normalization-table-rev.py").read())  # Does the same for reverse DNA chain
-exec (open(
+exec(open("./Analysis scripts/Normalization-table-rev.py").read())  # Does the same for reverse DNA chain
+exec(open(
     "./Analysis scripts/Normalization-column-dir.py").read())  # Generates normalized fragment frequencies for each chromosomal disc and calculates Pearson correlation between fragment frequencies and ectopic contacts frequencies (direct DNA strain)
-exec (open("./Analysis scripts/Normalization-column-rev.py").read())  # Does the same for reverse DNA chain
-exec (open(
+exec(open("./Analysis scripts/Normalization-column-rev.py").read())  # Does the same for reverse DNA chain
+exec(open(
     "./Analysis scripts/Column-combinations.py").read())  # Combines columns with ectopic contacts and fragment frequencies for both DNA chains
-exec (open(
+exec(open(
     "./Analysis scripts/Normalization-column-both.py").read())  # Calculates Pearson correlation between fragment frequencies and ectopic contacts frequencies (both DNA strain)
--- segmentanalysis/segmentsearch.py
+++ segmentanalysis/segmentsearch.py
@@ -1,5 +1,10 @@
 # This module contains all code ralated to search of small fragments in chromosome chunks
 import random
+import re
+
+
+class fragmentInfo:
+    count = 0
 
 
 def chooseFragments(segment, fragmentLength, fragmentDensity):
@@ -11,11 +16,28 @@ def chooseFragments(segment, fragmentLength, fragmentDensity):
     :return: list of generated random fragments
     """
     fragments = []
-    fragmentCount = len(segment) / fragmentDensity
+    fragmentCount = len(segment) // fragmentDensity
     for i in range(fragmentCount):
         begin = random.randrange(len(segment) - fragmentLength)  # random fragment beginning
         end = begin + fragmentLength  # random fragment end
         fragment = segment[begin:end]  # random fragment sequence
         fragments.append(fragment)
-        print(fragment)
     return fragments
+
+
+def searchFragment(sequence, fragment):
+    fragmentRe = re.compile(fragment)
+    positions = [m.start() for m in fragmentRe.finditer(sequence)]
+    return positions
+
+def countFragments(sequence, fragments, verbose=False):
+    nFragment = 0
+    counts = {}
+    nFragmentTotal = len(fragments)
+    for fragment in fragments:
+        fragmentCoordinates = searchFragment(sequence, fragment)
+        counts[fragment] = len(fragmentCoordinates)
+        nFragment += 1
+        if verbose and nFragment % 100 == 0:
+            print('Processed {}/{} fragments'.format(nFragment,nFragmentTotal))
+    return counts
