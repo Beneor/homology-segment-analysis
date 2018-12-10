ff18ea7f5b3d1030d7fa5c4838005363af0e05ee
Gennadii <Gennadiy.Zakharov@gmail.com>
2018-12-10 18:47:28 +0300

    Added executable to calculate correlation between ectopic contacts and homology intervals
    Former-commit-id: 7cb602d26f2d583f2814e6a9040cc8f817d39a4e

---
+++ genomecorrelation.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+programDescription = '''
+This script produces a set of short (5 - 30 by default) random fragments for the specified chromosome segment,
+splits chromosome into a set of long (10 kb by default) chunks,
+seeks random segments within chromosome parts,
+computes frequencies of occurence for segments of different length within chromosome parts,
+makes table for the normalized frequencies,
+and calculates Pearson correlation between fragment frequencies for chromosome discs and ectopic contact frequencies
+'''
+
+import argparse
+import sys
+from os.path import join, abspath, curdir
+
+import numpy as np
+from scipy.stats.stats import pearsonr
+
+# Adding local path to import program modules
+sys.path.append(abspath(join(curdir, 'segmentanalysis')))
+from segmentanalysis import segmentutils
+
+
+def readEctopics(ectopicsFileName):
+    ectopicsFile = open(ectopicsFileName)
+    ectopics = {}
+    for line in ectopicsFile:
+        ID, value = line.strip().split()
+        ectopics[ID] = float(value)
+    return ectopics
+
+
+# 0. ANALYZING INPUT PARAMETERS
+parser = argparse.ArgumentParser(description=programDescription, formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("homologyFileName", type=str,
+                    help="FASTA file (may be gzipped) containing genome sequence")
+parser.add_argument("ectopicsFileName", type=str,
+                    help="segment of chromosome to analyze (X:16113516:16900779) in BED-file notation (starting from 0, end is not included)")
+
+args = parser.parse_args()
+
+ectopics = readEctopics(args.ectopicsFileName)
+homology = segmentutils.readBedFile(args.homologyFileName)
+
+# Converting to numpy arrays
+
+homologyArr = [float(interval.value) for interval in homology]
+ectopicsArr = [ectopics[interval.ID] for interval in homology]
+
+print(homologyArr)
+print(ectopicsArr)
+
+corrCoef, pValue = pearsonr(np.array(homologyArr), np.array(ectopicsArr))
+print("Correlation coefficient: ", corrCoef)
+print("P-Value: ", pValue)
