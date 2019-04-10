9f5924a9e5697742b954ecc09322d7dd668db6fd
Gennadii <Gennadiy.Zakharov@gmail.com>
2019-04-10 17:11:42 +0300

    Implementes seguence analysis for one particular sequence
    Former-commit-id: b57cb166ec9e1bc0d89e950a9092aa24cfacf9a5

--- sequencecorrelation.py
+++ sequencecorrelation.py
@@ -1,155 +1,97 @@
 #!/usr/bin/env python3
 
+import argparse
+import sys
 import glob
-import math
+import re
 
-#I. Segment
-fileSuffix='-fr.txt'
-filesMask='*'+fileSuffix
-resultFiles = glob.glob(filesMask)
-seqsearch=open(r"Sequence-search.txt","w+")
-x="aattttcgcattttttgtaaggggtaacatcatcaaaatttgcaaaaaat"# INPUT THE SEQUENCE!!!
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats.stats import pearsonr, spearmanr
 
-# 2. Preparation of complementary segment
-c=""
-for i in range(0,len(x)):
-    j = int(len(x)-i-1)
-    s = x[j]
-    if s == "a":
-            c+="t"
-    elif s == "g":
-            c+="c"
-    elif s == "c":
-            c+="g"
-    elif s == "t":
-            c+="a"
-    else:
-            c+="n"
+from os.path import join, abspath, curdir
 
-#III. Segments search
-for fileName in resultFiles:
-    f=open(fileName, "r")  #Chromosomal parts files
-    for line in f:
-            a=str(line)
-            p=a.count(x) #Number of segment in direct DNA strain
-            q=a.count(c) #Number of segment in reverse DNA strain
-            summary=p+q #Summary number in both strains
-    print("%-12s%-17s"%(summary,f.name))
-    seqsearch.write(str(summary))
-    seqsearch.write(";")
-    seqsearch.write(f.name)
-    seqsearch.write("\n")
-    f.close()
-seqsearch.close()
+# Adding local path to import program modules
+sys.path.append(abspath(join(curdir, 'segmentanalysis')))
+from segmentanalysis import segmentutils,segmentstatistics
 
-#II. Sorting chromosome segments
-def sortnumber(line): #Sorting lines according to the files numbers in strings
-        e = str(line)
-        return int(e[e.rfind(";")+1:e.find("-")].strip())#The first segment position
-seqsearch=open(r"Sequence-search.txt","r")  # Initial order
-sortedMatchingFragments=open(r"Sequence-sort.txt","w+") # Sorted order
-bands=list()
-for line in seqsearch:
-    bands.append(line) #Creating list containing all strings
-    bands.sort(key=sortnumber) #Sorting values in the list
-for i in bands:
-    sortedMatchingFragments.write(i) #Writing valuse of the list as separate strings
-sortedMatchingFragments.close()
-seqsearch.close()
+programDescription = '''
 
-#III.Column preparation
-# 1. Frequencies assignment to discs
-ecffile=open(r"ECF.txt","r") #Chromosome bands borders and ECF values
-column=open(r"Sequence-sort.txt","r")
-newcolumn=open(r"Sequence-sort-ecf.txt","w+")
-newcolumn.write("Band;fragmentFrequency;ECF\n")
-for line in ecffile:
-        x=str(line)
-        band=x[0:x.find(",")]
-        a=int(x.find(",",x.find(",")+1))
-        start=int(x[x.find(",")+1:a])
-        end=int(x[a+1:x.rfind(",")])
-        startnum=round(start/10000)+1 #Number of the first chromosome fragment for the corresponding disc
-        endnum=round(end/10000)#Number of the last chromosome fragment for the corresponding disc
-        ecf=x[x.rfind(",")+1:].strip()#Ectopic contacts frequency
-        newcolumn.write(band), newcolumn.write(";")
-        sumfrfreq=0
-        column.seek(0)
-        num=0
-        for line in column:
-            y=str(line)
-            frfreq=float(y[0:y.find(";")])
-            num=int(y[y.find(";")+1:y.find("-")])
-            if num < startnum: continue
-            if num > endnum: continue
-            sumfrfreq+=frfreq #Summary fragment frequency for corresponding disc
-            num+=1 #Summary number of bands
-        dec=round(sumfrfreq,6)
-        newcolumn.write("{:0.10f}".format(dec))
-        newcolumn.write(";")
-        newcolumn.write(ecf)
-        newcolumn.write("\n")  
-column.close()
-newcolumn.close()
-ecffile.close()
+See details in program description in README.md  
+'''
 
-#IV. Pearson correlation calculation
+def readEctopics(ectopicsFileName):
+    """
+    Reads ectopics or nay similar genome data from tab-delimited file
+    :param ectopicsFileName: file name to read data
+    :return: dictionary of regions named adn regions data
+    """
+    ectopicsFile = open(ectopicsFileName)
+    ectopics = {}
+    for line in ectopicsFile:
+        ID, value = line.strip().split()
+        ectopics[ID] = float(value)
+    return ectopics
 
-newcolumn=open(r"Sequence-sort-ecf.txt","r")
-frfreq=0
-ecf=0
-n=0
-x1=0
-y1=0
-x4=0
-y4=0
-h2=0
-for line in newcolumn:
-    z = str(line)
-    if "Band" in z: continue
-    frfreq = float(z[z.find(";")+1:z.rfind(";")])
-    ecf = float(z[z.rfind(";")+1:])
-    x1+=frfreq #Sum of fragment frequencies*ect
-    y1+=ecf #Sum of fragment frequencies
-    n+=1
-avfr=x1/n #average fragment frequency
-avecf=y1/n #average ectopic contact frequency
-newcolumn.seek(0)
-for line in newcolumn:
-    z = str(line)
-    if "Band" in z: continue
-    frfreq = float(z[z.find(";")+1:z.rfind(";")])
-    ecf = float(z[z.rfind(";")+1:])
-    x2=frfreq-avfr
-    x3=math.pow(x2,2)
-    y2=ecf-avecf
-    y3=math.pow(y2,2)
-    h=x2*y2
-    x4+=x3 #Sum of squares
-    y4+=y3 #Sum of squares
-    h2+=h #Sum of multiplications
-cov = h2/(math.sqrt(x4*y4))
-print("\n")
-print("Pearson correlation with ECF:")
-print("Covariation coefficient: "), print(cov) #Pearson correlation
+def findSubstringWithOverlaps(sub, string):
+    return [m.start() for m in re.finditer(r'(?='+sub+')', string)]
 
-# Reliability of Pearson correlation
+# 0. ANALYZING INPUT PARAMETERS
+parser = argparse.ArgumentParser(description=programDescription, formatter_class=argparse.RawDescriptionHelpFormatter)
+# parser.add_argument("dataFolder", type=str,
+                    # help="Output data forlder for segmentanalysis run")
+parser.add_argument("fastaFileName", type=str,
+                    help="FASTA file (may be gzipped) containing genome sequence")
+parser.add_argument("fragment", type=str,
+                    help="sequence of fragment to find correlations")
+parser.add_argument("cytomap", type=str,
+                    help="BED file containing cytobands")
+parser.add_argument("ectopicsFileName", type=str,
+                    help="Tab-delimeted file containing ectopic contacts data")
+parser.add_argument("-v", "--verbose", action='store_true',
+                    help='Print additional information to stdout')
+parser.add_argument("-c", "--chunk", type=float, default=10.0,
+                    help='Chunk size to divide chromosome, in kilobases')
+parser.add_argument("-e", "--exclude", type=str,
+                    help='Coordinated of interval where we need to exclude signal, start:stop')
 
-tconf = cov*math.sqrt(n)/(1-math.pow(cov,2))
-print("t statistics: ")
-print(tconf)
-#Level of confidence at alpha = 5%
-#choosing Student coefficient for n and alpha
-if n >= 120:
-    trep = 1.98
-elif 60 <= n <120:
-    trep = 2.00
-elif 40 <=n < 120:
-    trep = 2.02
-else:
-    trep = 2.06
-if tconf >  trep: 
-    print("Reliable at p<0.05")
-else:
-    print("Unreliable at p<0.05")
+args = parser.parse_args()
 
+#I. Arguments parsing
+
+#III. Collecting locations
+
+if args.verbose:
+    print('Loading genome from FASTA file')
+genome = segmentutils.readFasta(segmentutils.openMaybeGzipped(args.fastaFileName))
+
+chunkSize = int(args.chunk * 1000)
+
+cytomap = segmentutils.readBedFile(args.cytomap)
+
+chrPositionsChunks={}
+
+for chromosome in genome:
+    # Now we search only for single fragment - no need to use Aho-Corasick search
+    fragmentPositionsFor = np.fromiter(findSubstringWithOverlaps(args.fragment,                       genome[chromosome]), dtype=np.int64)
+    fragmentPositionsRev = np.fromiter(findSubstringWithOverlaps(segmentutils.revcomp(args.fragment), genome[chromosome]), dtype=np.int64)
+    if args.verbose:
+        print("Chromosome {} (forward) - {} occurences".format(chromosome, len(fragmentPositionsFor)))
+        print("Chromosome {} (reverse) - {} occurences".format(chromosome, len(fragmentPositionsRev)))
+    fragmentPositions = np.concatenate((fragmentPositionsFor, fragmentPositionsRev))
+    fragmentPositions.sort()
+    chrPositionsChunks[chromosome] = fragmentPositions // chunkSize  # Dividing each element by chunk size using numpy syntax
+
+counts = segmentstatistics.chunksToCounts(chrPositionsChunks, genome, chunkSize)
+if args.exclude is not None:
+    exclude = segmentutils.strToBed(args.exclude, separator=':')
+    if args.verbose:
+        print('Setting to zero counts for chunks {}:{}-{}'.format(exclude.chromosome,
+              exclude.start // chunkSize, exclude.stop // chunkSize))
+    segmentstatistics.excludeIntervalFromCounts(counts, exclude, chunkSize)
+
+cytoCounts = segmentstatistics.countsToCytomap(cytomap, counts, chunkSize)
+if args.verbose:
+    print('Total matches: ', np.sum(cytoCounts))
+cytoCountsFileName = '{}.{}.cytocounts.txt'.format(args.fastaFileName, args.fragment)
+segmentutils.dumpCytoCouns(cytoCountsFileName, cytomap, cytoCounts)
