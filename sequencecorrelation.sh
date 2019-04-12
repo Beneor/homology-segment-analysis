8fea77902ef75d2f6acd1aa80d07bd0a0d8fbe34
Gennadii <Gennadiy.Zakharov@gmail.com>
2019-04-12 17:36:38 +0300

    Fixed variable name
    Former-commit-id: 50a33e80acc1e83d3af68adce8372031f751d91a

--- sequencecorrelation.sh
+++ sequencecorrelation.sh
@@ -5,7 +5,7 @@ set -e
 fragment=${1}
 ./sequencecorrelation.py -v -e X:11982050:12772070 examples/dm6.nounmapped.fa.gz ${fragment} examples/DmelMapTable.160615c.bed
 for corr in Pearson Spearman; do
-    ./genomecorrelation.py -m -c X -r $corr  examples/dm6.nounmapped.fa.gz.${fragment}.cytocounts.txt examples/Berlin.ectopics.tsv -s ${fragment}.corr.png
+    ./genomecorrelation.py -m -c X -r $corr  examples/dm6.nounmapped.fa.gz.${fragment}.cytocounts.txt examples/Berlin.ectopics.tsv -s ${fragment}.$corr.png
 done
 
 
